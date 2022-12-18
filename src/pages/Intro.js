4cdfdc77a21fce07fdac6739069003e7ccb85b2f
IsabelGuMo <gm.isabel@gmail.com>
2022-12-18 20:47:30 +0100

    Fixed Intro.js tag

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -10,14 +10,10 @@ export default function Intro() {
       <Header/>
       <div className="intro-container">
         <p> Sakura Kinomoto, una pequeña niña de diez años que accidentalmente libera un conjunto de cartas que se encontraban dentro de un libro, el cual había descubierto en la biblioteca de su padre. Tras esto, Kero —el guardián del libro— emerge y, al enterarse de que la mayoría de las cartas conocidas como las «cartas Clow» se habían esparcido por todas partes, le dice a Sakura que ahora es su responsabilidad convertirse en una «cardcaptor» e ir en busca de ellas para evitar que ocurra una «catástrofe» en el mundo. Por lo tanto, Sakura adquiere habilidades especiales para así enfrentarse a los poderes mágicos de cada carta y lograr capturarlas y sellarlas</p>
-<<<<<<< HEAD
-      </div>
-    </>
-=======
         <Link to="/">
           <Button class='frame' text='Go to Cards' />
         </Link>
-    </div>
->>>>>>> b7aa6d7 (Create Button Component)
+      </div>
+    </>
   )
 }
