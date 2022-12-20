6bd5dfe52a4ecde0eb169f8312db83dbba67d807
AmelieLT <amelie_letron@yahoo.fr>
2022-12-20 15:29:21 +0100

    usestate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Link } from 'react-router-dom';
 import './App.css';
+import Button from './components/Button';
 import FlippableCard from './components/Flippable-card';
 import Header from './components/Header';
 
 function App() {
-
+//estado
   return (
     <div className="App">
       <Header/>
       <Link to='/intro'>Voy a la página principal</Link>
+      <Link to='/meaning'>
+        <Button className='frame'text='Descubre el significado de tus cartas'/>
+      </Link>
       <FlippableCard/>  
     </div>
   );
