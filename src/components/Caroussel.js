6790dbf57ac5fb28da588058284a61a350424a82
AmelieLT <amelie_letron@yahoo.fr>
2022-12-21 09:51:17 +0100

    useEffect api

--- src/components/Caroussel.js
+++ src/components/Caroussel.js
@@ -1,12 +1,17 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import GetCards from '../services/GetCards'
 import Card from '../components/Card'
 import '../styles/Styles.css'
 
 function Caroussel() {
     const [cards, setCards] = useState(null);
-    GetCards(setCards); //cuando se importa la funcion fetch eluseState cambia de estado
-    return (
+   
+     //cuando se importa la funcion fetch eluseState cambia de estado
+    useEffect(() => {
+        GetCards(setCards)
+    },[])
+
+     return (
         <>    
             {cards !== null ? cards.map((card, index) => (
                <Card card={card} key={index}
